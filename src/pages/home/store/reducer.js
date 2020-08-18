667b6c798152427b90dc016fee71fb53fe482222
pluscai <3114658034@qq.com>
2020-08-18 15:08:20 +0800

    8-3&4&5 首页专题区域布局及reducer的设计 & 文章列表 & 推荐组件

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -30,6 +30,13 @@ const defaultState = fromJS({
         title: '胡歌12年后首谈车祸：既然活下来了，就不能白白活着',
         desc: '文/麦大人 01 胡歌又刷屏了。 近日他上了《朗读者》，而这一期的主题是“生命”，他用磁性的嗓音，朗读了一段《哈姆雷特》中的经典独白，相当震撼：...',
         imgUrl: 'https://upload-images.jianshu.io/upload_images/18763524-a7b10ae3853800e1?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+    }],
+    recommendList: [{
+        id: 1,
+        imgUrl: "http://cdn2.jianshu.io/assets/web/banner-s-3-7123fd94750759acf7eca05b871e9d17.png"
+    }, {
+        id: 2,
+        imgUrl: "http://cdn2.jianshu.io/assets/web/banner-s-5-4ba25cf5041931a0ed2062828b4064cb.png"
     }]
 });
 
