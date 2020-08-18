8532b973a9d50dacc5998568e9e386ea7acb71cf
pluscai <3114658034@qq.com>
2020-08-18 13:59:06 +0800

    8-4 首页文章列表制作

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,6 +9,27 @@ const defaultState = fromJS({
         id: 2,
         title: '手绘',
         imgUrl: '//upload.jianshu.io/collections/images/283250/%E6%BC%AB%E7%94%BB%E4%B8%93%E9%A2%98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
+    }],
+    articleList: [{
+        id: 1,
+        title: '胡歌12年后首谈车祸：既然活下来了，就不能白白活着',
+        desc: '文/麦大人 01 胡歌又刷屏了。 近日他上了《朗读者》，而这一期的主题是“生命”，他用磁性的嗓音，朗读了一段《哈姆雷特》中的经典独白，相当震撼：...',
+        imgUrl: 'https://upload-images.jianshu.io/upload_images/18763524-a7b10ae3853800e1?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+    },{
+        id: 2,
+        title: '胡歌12年后首谈车祸：既然活下来了，就不能白白活着',
+        desc: '文/麦大人 01 胡歌又刷屏了。 近日他上了《朗读者》，而这一期的主题是“生命”，他用磁性的嗓音，朗读了一段《哈姆雷特》中的经典独白，相当震撼：...',
+        imgUrl: 'https://upload-images.jianshu.io/upload_images/18763524-a7b10ae3853800e1?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+    },{
+        id: 3,
+        title: '胡歌12年后首谈车祸：既然活下来了，就不能白白活着',
+        desc: '文/麦大人 01 胡歌又刷屏了。 近日他上了《朗读者》，而这一期的主题是“生命”，他用磁性的嗓音，朗读了一段《哈姆雷特》中的经典独白，相当震撼：...',
+        imgUrl: 'https://upload-images.jianshu.io/upload_images/18763524-a7b10ae3853800e1?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+    },{
+        id: 4,
+        title: '胡歌12年后首谈车祸：既然活下来了，就不能白白活着',
+        desc: '文/麦大人 01 胡歌又刷屏了。 近日他上了《朗读者》，而这一期的主题是“生命”，他用磁性的嗓音，朗读了一段《哈姆雷特》中的经典独白，相当震撼：...',
+        imgUrl: 'https://upload-images.jianshu.io/upload_images/18763524-a7b10ae3853800e1?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
     }]
 });
 
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -48,3 +48,31 @@ export const TopicItem = styled.div`
 	}
 `;
 
+export const ListItem = styled.div`
+	overflow: hidden;
+	padding: 20px 0;
+	border-bottom: 1px solid #dcdcdc;
+	.pic {
+		display: block;
+		width: 125px;
+		height: 100px;
+		float: right;
+		border-radius: 10px;
+	}
+`;
+
+export const ListInfo =	styled.div`
+	width: 500px;
+	float: left;
+	.title {
+		line-height: 27px;
+		font-size: 18px;
+		font-weight: bold;
+		color: #333;
+	}
+	.desc {
+		line-height: 24px;
+		font-size: 13px;
+		color: #999;
+	}
+`;
