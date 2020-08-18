1500607c74b65ba4245b639bec3839a52072f176
pluscai <3114658034@qq.com>
2020-08-18 17:29:43 +0800

    8-7 异步操作代码拆分优化

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,8 @@ import Topic from "./components/Topic";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
-import axios from 'axios';
 import { connect } from 'react-redux';
+import { actionCreators } from './store'
 
 import {
     HomeWrapper,
@@ -30,23 +30,15 @@ class Home extends Component{
     }
 
     componentDidMount() {
-        axios.get('/api/home.json').then((res) => {
-            const result = res.data.data;
-            const action = {
-                type: 'change_home_data',
-                topicList: result.topicList,
-                articleList: result.articleList,
-                recommendList: result.recommendList,
-            };
-            this.props.changeHomeData(action)
-        })
+        this.props.changeHomeData();
     }
 }
 
 const mapDispatch = (dispatch) => {
     return {
-        changeHomeData(action) {
-            dispatch(action)
+        changeHomeData() {
+            const action = actionCreators.getHomeInfo();
+            dispatch(action);
         }
     }
 }
---
+++ src/pages/home/store/actionCreators.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import * as constants from './constants';
+
+const changeHomeData = (result) => {
+    return {
+        type: constants.CHANGE_HOME_DATA,
+        topicList: result.topicList,
+        articleList: result.articleList,
+        recommendList: result.recommendList,
+    }
+}
+
+export const getHomeInfo = () => {
+    return (dispatch) => {
+        axios.get('/api/home.json').then((res) => {
+            const result = res.data.data;
+            const action = changeHomeData(result);
+            dispatch(action)
+        })
+    }
+}
