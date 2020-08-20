6c7fe063817b819b8e421b32f360d2cc6923fb20
pluscai <3114658034@qq.com>
2020-08-20 16:26:28 +0800

    add build

---
+++ build/precache-manifest.e3ccfa04a72a0804b0ec984839d8bce9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd1a56d699c0d24ff7133a61cd850b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "110eae955fa9355cf381",
+    "url": "/static/js/2.237cac37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.237cac37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9287d242947864ae58",
+    "url": "/static/js/3.36d6bbf6.chunk.js"
+  },
+  {
+    "revision": "7bb155b6e5e72e181beb",
+    "url": "/static/js/main.42de4e87.chunk.js"
+  },
+  {
+    "revision": "3e41c14b398a238b698f",
+    "url": "/static/js/runtime-main.75503f44.js"
+  },
+  {
+    "revision": "040e552548a4b8a145ca4d6d90e9a078",
+    "url": "/static/media/iconfont.040e5525.svg"
+  },
+  {
+    "revision": "1c744d9c6edaf9d42b0164cf1f23ece2",
+    "url": "/static/media/iconfont.1c744d9c.eot"
+  },
+  {
+    "revision": "7d96e9151e35d6f0d816736bb6d1c388",
+    "url": "/static/media/iconfont.7d96e915.ttf"
+  }
+]);
